742c5307a86fa6b06c3032525b51ff0b8d6760c8
Dénes Solti <sodnaatx@gmail.com>
2020-01-07 14:27:41 +0100

    Assembly.cs -> AssemblyAttributes.cs

--- SRC/Properties/AssemblyAttributes.cs
+++ SRC/Properties/AssemblyAttributes.cs
@@ -1,8 +1,16 @@
 ﻿/********************************************************************************
-* Assembly.cs                                                                   *
+* AssemblyAttributes.cs                                                         *
 *                                                                               *
 * Author: Denes Solti                                                           *
 ********************************************************************************/
 using System.Resources;
+using System.Runtime.CompilerServices;
 
-[assembly: NeutralResourcesLanguage("en")]
+[
+    assembly:
+        NeutralResourcesLanguage("en")
+#if DEBUG
+        , 
+        InternalsVisibleTo("ProxyGen.Tests")
+#endif
+]
