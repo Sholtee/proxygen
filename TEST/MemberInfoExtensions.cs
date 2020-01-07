ffe44bafd6ab16b06595c7ab839e56e7e4f044bd
Dénes Solti <sodnaatx@gmail.com>
2020-01-07 15:16:37 +0100

    MemberInfoExtensions tests

---
+++ TEST/MemberInfoExtensions.cs
@@ -0,0 +1,23 @@
+﻿/********************************************************************************
+* MemberInfoExtensions.cs                                                       *
+*                                                                               *
+* Author: Denes Solti                                                           *
+********************************************************************************/
+using System.Collections.Generic;
+
+using NUnit.Framework;
+
+namespace Solti.Utils.Proxy.Internals.Tests
+{
+    [TestFixture]
+    public class MemberInfoExtensionsTests
+    {
+        [Test]
+        public void GetFullName_ShouldDoWhatTheNameSuggests() 
+        {
+            Assert.That(GetType().GetMethod(nameof(GetFullName_ShouldDoWhatTheNameSuggests)).GetFullName(), Is.EqualTo("Solti.Utils.Proxy.Internals.Tests.MemberInfoExtensionsTests.GetFullName_ShouldDoWhatTheNameSuggests"));
+            Assert.That(typeof(List<>).GetProperty(nameof(List<object>.Count)).GetFullName(), Is.EqualTo("System.Collections.Generic.List<T>.Count"));
+            Assert.That(typeof(List<object>).GetProperty(nameof(List<object>.Count)).GetFullName(), Is.EqualTo("System.Collections.Generic.List<System.Object>.Count"));
+        }
+    }
+}
