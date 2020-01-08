f74923634456631f283ae1d0b18926a1996abd98
Dénes Solti <sodnaatx@gmail.com>
2020-01-08 19:23:04 +0100

    MemberInfoExtensions.IsStatic()

--- SRC/Private/Extensions/MemberInfoExtensions.cs
+++ SRC/Private/Extensions/MemberInfoExtensions.cs
@@ -3,6 +3,8 @@
 *                                                                               *
 * Author: Denes Solti                                                           *
 ********************************************************************************/
+using System;
+using System.Diagnostics;
 using System.Reflection;
 
 namespace Solti.Utils.Proxy.Internals
@@ -10,5 +12,23 @@ namespace Solti.Utils.Proxy.Internals
     internal static class MemberInfoExtensions
     {
         public static string GetFullName(this MemberInfo src) => $"{ProxySyntaxGeneratorBase.CreateType(src.DeclaringType).ToFullString()}.{src.Name}";
+
+        public static bool IsStatic(this MemberInfo src) 
+        {
+            switch (src)
+            {
+                case MethodInfo method:
+                    return method.IsStatic;
+                case FieldInfo field:
+                    return field.IsStatic;
+                case PropertyInfo property:
+                    return (property.GetMethod ?? property.SetMethod ?? throw new InvalidOperationException()).IsStatic;
+                case EventInfo @event:
+                    return (@event.AddMethod ?? @event.RemoveMethod ?? throw new InvalidOperationException()).IsStatic;
+                default:
+                    Debug.Fail("Unknown member type");
+                    return false;
+            }
+        }
     }
 }
--- TEST/MemberInfoExtensions.cs
+++ TEST/MemberInfoExtensions.cs
@@ -3,7 +3,9 @@
 *                                                                               *
 * Author: Denes Solti                                                           *
 ********************************************************************************/
+using System;
 using System.Collections.Generic;
+using System.Reflection;
 
 using NUnit.Framework;
 
@@ -12,12 +14,28 @@ namespace Solti.Utils.Proxy.Internals.Tests
     [TestFixture]
     public class MemberInfoExtensionsTests
     {
-        [Test]
-        public void GetFullName_ShouldDoWhatTheNameSuggests() 
+        public static (MemberInfo Member, string Expected)[] Members = new[] 
         {
-            Assert.That(GetType().GetMethod(nameof(GetFullName_ShouldDoWhatTheNameSuggests)).GetFullName(), Is.EqualTo("Solti.Utils.Proxy.Internals.Tests.MemberInfoExtensionsTests.GetFullName_ShouldDoWhatTheNameSuggests"));
-            Assert.That(typeof(List<>).GetProperty(nameof(List<object>.Count)).GetFullName(), Is.EqualTo("System.Collections.Generic.List<T>.Count"));
-            Assert.That(typeof(List<object>).GetProperty(nameof(List<object>.Count)).GetFullName(), Is.EqualTo("System.Collections.Generic.List<System.Object>.Count"));
-        }
+            ((MemberInfo) typeof(MemberInfoExtensionsTests).GetMethod(nameof(GetFullName_ShouldDoWhatTheNameSuggests)), "Solti.Utils.Proxy.Internals.Tests.MemberInfoExtensionsTests.GetFullName_ShouldDoWhatTheNameSuggests"),
+            ((MemberInfo) typeof(List<>).GetProperty(nameof(List<object>.Count)), "System.Collections.Generic.List<T>.Count"),
+            ((MemberInfo) typeof(List<object>).GetProperty(nameof(List<object>.Count)), "System.Collections.Generic.List<System.Object>.Count")
+        };
+
+        [TestCaseSource(nameof(Members))]
+        public void GetFullName_ShouldDoWhatTheNameSuggests((MemberInfo Member, string Expected) param) =>
+            Assert.That(param.Member.GetFullName(), Is.EqualTo(param.Expected));
+
+        public static (MemberInfo Member, bool IsStatic)[] StaticNonStatic = new[]
+        {
+            ((MemberInfo) typeof(AppDomain).GetProperty(nameof(AppDomain.CurrentDomain), BindingFlags.Static | BindingFlags.Public), true),
+            ((MemberInfo) typeof(AppDomain).GetMethod(nameof(AppDomain.GetCurrentThreadId), BindingFlags.Static | BindingFlags.Public), true),
+            ((MemberInfo) typeof(MemberInfoExtensionsTests).GetMethod(nameof(GetFullName_ShouldDoWhatTheNameSuggests)), false),
+            ((MemberInfo) typeof(List<>).GetProperty(nameof(List<object>.Count)), false),
+            ((MemberInfo) typeof(List<object>).GetProperty(nameof(List<object>.Count)), false)
+        };
+
+        [TestCaseSource(nameof(StaticNonStatic))]
+        public void IsStatic_ShouldDoWhatTheNameSuggests((MemberInfo Member, bool IsStatic) param)
+            => Assert.That(param.Member.IsStatic(), Is.EqualTo(param.IsStatic));
     }
 }
