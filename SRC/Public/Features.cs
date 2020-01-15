b63829fdb645f3ab5771c5cadbe5c1293f77b8aa
Dénes Solti <sodnaatx@gmail.com>
2020-01-15 17:28:50 +0100

    Features.cs: typo

--- SRC/Public/Features.cs
+++ SRC/Public/Features.cs
@@ -15,7 +15,7 @@ namespace Solti.Utils.Proxy
         internal const LanguageVersion LV_MAX_SUPPORTED = Microsoft.CodeAnalysis.CSharp.LanguageVersion.CSharp7;
 
         /// <summary>
-        /// The language version for which this libary was optimised.
+        /// The language version for which this library was optimised.
         /// </summary>
         /// <remarks>Apps targeting newer version can also use this library but there is no guarantee for proper working.</remarks>
         public static string LanguageVersion { get; } = LV_MAX_SUPPORTED.ToString();
