b23f1d592e0d9b3be1715fcf82764b244b43e790
Dénes Solti <sodnaatx@gmail.com>
2020-01-25 14:08:28 +0100

    DuckBase.cs cosmetics

--- SRC/Private/DuckBase.cs
+++ SRC/Private/DuckBase.cs
@@ -22,12 +22,7 @@ namespace Solti.Utils.Proxy.Internals
         /// Creates a new <see cref="DuckBase{T}"/> instance.
         /// </summary>
         /// <param name="target">The target of the entity being created.</param>
-        public DuckBase(T target)  // ne "protected" legyen
-        {
-            if (target == null)
-                throw new ArgumentNullException(nameof(target));
-
-            Target = target;
-        }
+        public DuckBase(T target) =>  // ne "protected" legyen
+            Target = target != null ? target : throw new ArgumentNullException(nameof(target)); // Target = target ?? throw new... csak C# 8+-tol van
     }
 }
