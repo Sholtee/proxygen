8fcc08f607f4f8aa34a0f34053b7edad36a445ed
Dénes Solti <sodnaatx@gmail.com>
2020-04-02 14:05:43 +0200

    greenify tests

--- SRC/Private/Extensions/ParameterInfoExtensions.cs
+++ SRC/Private/Extensions/ParameterInfoExtensions.cs
@@ -4,67 +4,51 @@
 * Author: Denes Solti                                                           *
 ********************************************************************************/
 using System;
-using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
 namespace Solti.Utils.Proxy.Internals
 {
     internal static class ParameterInfoExtensions
     {
-        private static readonly HashSet<UnmanagedType> NativeByRefIndicators = new HashSet<UnmanagedType> 
-        {
-            UnmanagedType.LPArray
-        };
-
         public static ParameterKind GetParameterKind(this ParameterInfo src) 
         {
-            if (src.IsOut)
+            if (src.ParameterType.IsByRef)
             {
                 //
-                // Ha a parameter csak nativ kontextusban kimeneti akkor az minket nem erdekel.
+                // Innentol "native by ref" (pl. IntPtr) nem jatszik.
                 //
 
-                if (IsNativeByRef()) return ParameterKind.None;
-
-                return ParameterKind.Out;
-            }
-
-            if (src.IsIn) 
-            {
-                if (IsNativeByRef()) return ParameterKind.None;
-
-#if !NETSTANDARD2_0
+#if NETSTANDARD2_0
                 //
-                // Ha nincs "IsReadOnlyAttribute" akkor "ref ValueType" a parameter tipusa.
+                // "IsReadOnlyAttribute" csak netstandard2.1-tol kezdve publikus.
                 //
 
-                if (src.GetCustomAttribute<IsReadOnlyAttribute>() != null)
+                if (src.IsIn && src.GetCustomAttributes().Any(attr => attr.GetType().FullName == "System.Runtime.CompilerServices.IsReadOnlyAttribute"))
+#else
+                //
+                // "src.IsIn" nem eleg
+                //
+
+                if (src.IsIn && src.GetCustomAttribute<IsReadOnlyAttribute>() != null)
 #endif
-                return ParameterKind.In;
+                    return ParameterKind.In;
+
+                if (src.IsOut)
+                    return ParameterKind.Out;
+
+                return ParameterKind.InOut;
             }
-
-            //
-            // "ParameterType.IsByRef" param.Is[In|Out] eseten is igazat ad vissza -> a lenti feltetel In|Out vizsgalat utan szerepeljen.
-            //
-
-            if (src.ParameterType.IsByRef) return ParameterKind.InOut;
 
             //
             // "params" es referencia szerinti parameter atadas egymast kizaroak.
             //
 
-            if (src.GetCustomAttribute<ParamArrayAttribute>() != null) return ParameterKind.Params;
+            if (src.GetCustomAttribute<ParamArrayAttribute>() != null) 
+                return ParameterKind.Params;
 
             return ParameterKind.None;
-
-            bool IsNativeByRef() 
-            {
-                var @as = src.GetCustomAttribute<MarshalAsAttribute>();
-
-                return @as != null && NativeByRefIndicators.Contains(@as.Value);
-            }
         }
     }
 }
