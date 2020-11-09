0a4d405514912005631a04589b5a044e807bf86a
Dénes Solti <sodnaatx@gmail.com>
2020-11-09 19:38:55 +0100

    greenify test

--- TEST/ProxyGen.Tests/InterfaceInterceptor.cs
+++ TEST/ProxyGen.Tests/InterfaceInterceptor.cs
@@ -25,7 +25,7 @@ namespace Solti.Utils.Proxy.Tests
         [Test]
         public void ResolveMember_ShouldResolveTheMemberByToken() 
         {
-            PropertyInfo prop = typeof(IDescendant).ListMembers<PropertyInfo>().Single(prop => prop.Name == nameof(IDescendant.Prop)); // GetProperty() nem mukodne mert az osbol kerdezzuk le
+            PropertyInfo prop = typeof(IDescendant).ListMembers<PropertyInfo>().Single(x => x.Name == nameof(IDescendant.Prop)); // GetProperty() nem mukodne mert az osbol kerdezzuk le
             Assert.AreSame(prop, InterfaceInterceptor<IDescendant>.ResolveMember(prop.MetadataToken));
         }
     }
