e6e56a2010558f60db5129b41a3245e9757c411e
Dénes Solti <sodnaatx@gmail.com>
2020-11-15 19:08:09 +0100

    SyntaxFactoryBase.cs cleanup

--- SRC/Private/SyntaxFactories/SyntaxFactoryBase.cs
+++ SRC/Private/SyntaxFactories/SyntaxFactoryBase.cs
@@ -5,7 +5,6 @@
 ********************************************************************************/
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Reflection;
 using System.Threading;
@@ -22,16 +21,7 @@ namespace Solti.Utils.Proxy.Internals
 
     internal abstract class SyntaxFactoryBase
     {
-        private readonly HashSet<Assembly> FReferences = new HashSet<Assembly>();
-        protected readonly List<MemberDeclarationSyntax> FMembers = new List<MemberDeclarationSyntax>();
-
-        public IReadOnlyCollection<MemberDeclarationSyntax> Members => FMembers;
-
-        public IReadOnlyCollection<MetadataReference> GetReferences() => FReferences
-            .Select(asm => MetadataReference.CreateFromFile(asm.Location))
-            .ToArray();
-
-        public virtual string GeneratedClassName { get; } = "GeneratedProxy";
+        private readonly HashSet<Assembly> FReferences = new HashSet<Assembly>(Runtime.Assemblies);
 
         protected void AddReference(Type type) 
         {
@@ -47,165 +37,6 @@ namespace Solti.Utils.Proxy.Internals
                 AddReference(genericArg);
 
             FReferences.Add(asm);
-        }
-
-        protected internal virtual ConstructorDeclarationSyntax DeclareCtor(ConstructorInfo ctor) => ConstructorDeclaration
-            (
-                identifier: Identifier(GeneratedClassName)
-            )
-            .WithModifiers
-            (
-                modifiers: TokenList
-                (
-                    Token(SyntaxKind.PublicKeyword)
-                )
-            )
-            .WithParameterList
-            (
-                parameterList: ParameterList(ctor.GetParameters().ToSyntaxList(param => Parameter
-                (
-                    identifier: Identifier(param.Name)
-                )
-                .WithType
-                (
-                    type: CreateType(param.ParameterType)
-                )))
-            );
-
-        protected internal virtual MethodDeclarationSyntax DeclareMethod(MethodInfo method)
-        {
-            Type
-                declaringType = method.DeclaringType,
-                returnType = method.ReturnType;
-
-            Debug.Assert(declaringType.IsInterface);
-
-            TypeSyntax returnTypeSytax = CreateType(returnType);
-
-            if (returnType.IsByRef)
-                returnTypeSytax = RefType(returnTypeSytax);
-
-            MethodDeclarationSyntax result = MethodDeclaration
-            (
-                returnType: returnTypeSytax,
-                identifier: Identifier(method.StrippedName())
-            )
-            .WithExplicitInterfaceSpecifier
-            (
-                explicitInterfaceSpecifier: ExplicitInterfaceSpecifier((NameSyntax) CreateType(declaringType))
-            )
-            .WithParameterList
-            (
-                ParameterList
-                (
-                    parameters: method.GetParameters().ToSyntaxList(param =>
-                    {
-                        ParameterSyntax parameter = Parameter(Identifier(param.Name)).WithType
-                        (
-                            type: CreateType(param.ParameterType)
-                        );
-
-                        List<SyntaxKind> modifiers = new List<SyntaxKind>();
-
-                        switch (param.GetParameterKind())
-                        {
-                            case ParameterKind.In:
-                                modifiers.Add(SyntaxKind.InKeyword);
-                                break;
-                            case ParameterKind.Out:
-                                modifiers.Add(SyntaxKind.OutKeyword);
-                                break;
-                            case ParameterKind.InOut:
-                                modifiers.Add(SyntaxKind.RefKeyword);
-                                break;
-                            case ParameterKind.Params:
-                                modifiers.Add(SyntaxKind.ParamsKeyword);
-                                break;
-                        }
-
-                        if (modifiers.Any()) parameter = parameter.WithModifiers
-                        (
-                            TokenList(modifiers.Select(Token))
-                        );
-
-                        return parameter;
-                    })
-                )
-            );
-
-            if (method.IsGenericMethod) result = result.WithTypeParameterList // kulon legyen kulomben lesz egy ures "<>"
-            (
-                typeParameterList: TypeParameterList
-                (
-                    parameters: method.GetGenericArguments().ToSyntaxList(type => TypeParameter(CreateType(type).ToFullString()))
-                )
-            );
-
-            return result;
-        }
-
-        protected internal virtual PropertyDeclarationSyntax DeclareProperty(PropertyInfo property)
-        {
-            Debug.Assert(property.DeclaringType.IsInterface);
-
-            return PropertyDeclaration
-            (
-                type: CreateType(property.PropertyType),
-                identifier: Identifier(property.StrippedName())
-            )
-            .WithExplicitInterfaceSpecifier
-            (
-                explicitInterfaceSpecifier: ExplicitInterfaceSpecifier((NameSyntax) CreateType(property.DeclaringType))
-            );
-        }
-
-        protected internal virtual IndexerDeclarationSyntax DeclareIndexer(PropertyInfo property)
-        {
-            Debug.Assert(property.DeclaringType.IsInterface);
-            Debug.Assert(property.IsIndexer());
-
-            ParameterInfo[] indices = property.GetIndexParameters();
-
-            return IndexerDeclaration
-            (
-                type: CreateType(property.PropertyType)
-            )
-            .WithExplicitInterfaceSpecifier
-            (
-                explicitInterfaceSpecifier: ExplicitInterfaceSpecifier((NameSyntax) CreateType(property.DeclaringType))
-            )
-            .WithParameterList
-            (
-                parameterList: BracketedParameterList
-                (
-                    parameters: indices.ToSyntaxList
-                    (
-                        index => Parameter
-                        (
-                            identifier: Identifier(index.Name)
-                        )
-                        .WithType
-                        (
-                            type: CreateType(index.ParameterType)
-                        )
-                    )
-                )
-            );
-        }
-
-        protected internal virtual EventDeclarationSyntax DeclareEvent(EventInfo @event)
-        {
-            Debug.Assert(@event.DeclaringType.IsInterface);
-
-            return EventDeclaration
-            (
-                type: CreateType(@event.EventHandlerType),
-                identifier: Identifier(@event.StrippedName())
-            )
-            .WithExplicitInterfaceSpecifier
-            (
-                explicitInterfaceSpecifier: ExplicitInterfaceSpecifier((NameSyntax) CreateType(@event.DeclaringType))
-            );
         }
 
         protected internal virtual TypeSyntax CreateType(Type type) 
@@ -281,25 +112,14 @@ namespace Solti.Utils.Proxy.Internals
             );
         }
 
-        public virtual void Setup(Type iface, CancellationToken cancellation) 
-        {
-            foreach (MemberInfo member in iface.ListMembers<MemberInfo>()) 
-            {
-                cancellation.ThrowIfCancellationRequested();
+        protected abstract CompilationUnitSyntax GenerateProxyUnit(CancellationToken cancellation);
 
-                switch (member) 
-                {
-                    case MethodInfo method:
-                        FMembers.Add(DeclareMethod(method));
-                        break;
-                    case PropertyInfo property:
-                        FMembers.Add(property.IsIndexer() ? DeclareIndexer(property) : (MemberDeclarationSyntax) DeclareProperty(property));
-                        break;
-                    case EventInfo evt:
-                        FMembers.Add(DeclareEvent(evt));
-                        break;
-                }
-            }
-        }
+        public (CompilationUnitSyntax Unit, IReadOnlyCollection<MetadataReference> References) GetContext(CancellationToken cancellation = default) => 
+        (
+            GenerateProxyUnit(cancellation),
+            FReferences
+                .Select(asm => MetadataReference.CreateFromFile(asm.Location))
+                .ToArray()
+        );
     }
 }
