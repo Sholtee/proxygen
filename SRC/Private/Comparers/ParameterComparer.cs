191f7f56660fd0a68b10df701e82ed733a1f4e6f
Dénes Solti <sodnaatx@gmail.com>
2020-11-15 07:57:24 +0100

    xXxComparer.cs: remove useless SuppressMessage

--- SRC/Private/Comparers/ParameterComparer.cs
+++ SRC/Private/Comparers/ParameterComparer.cs
@@ -3,14 +3,12 @@
 *                                                                               *
 * Author: Denes Solti                                                           *
 ********************************************************************************/
-using System.Diagnostics.CodeAnalysis;
 using System.Reflection;
 
 namespace Solti.Utils.Proxy.Internals
 {
     using Primitives;
 
-    [SuppressMessage("Performance", "CA1812:Avoid uninstantiated internal classes", Justification = "The class is passed as a type parameter that has a new constraint.")]
     internal sealed class ParameterComparer : ComparerBase<ParameterComparer, ParameterInfo>
     {
         public override int GetHashCode(ParameterInfo obj) => new
