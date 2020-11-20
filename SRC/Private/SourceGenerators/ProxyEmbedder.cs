2cd3f3574bd357b5d1559ec3aa786f5d99b9c300
Dénes Solti <sodnaatx@gmail.com>
2020-11-20 07:54:26 +0100

    ProxyEmbedder: introduce GetAttributes() method

---
+++ SRC/Private/SourceGenerators/ProxyEmbedder.cs
@@ -0,0 +1,37 @@
+﻿/********************************************************************************
+* ProxyEmbedder.cs                                                              *
+*                                                                               *
+* Author: Denes Solti                                                           *
+********************************************************************************/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace Solti.Utils.Proxy.Internals
+{
+    internal class ProxyEmbedder
+    {
+        public static IEnumerable<AttributeSyntax> GetAttributes<TAttribute>(CSharpCompilation compilation, CancellationToken cancellation = default) where TAttribute : Attribute
+        {
+            foreach (SyntaxTree syntaxTree in compilation.SyntaxTrees)
+            {
+                SemanticModel sm = compilation.GetSemanticModel(syntaxTree);
+
+                foreach (AttributeSyntax node in syntaxTree.GetRoot(cancellation).DescendantNodes().OfType<AttributeSyntax>())
+                {
+                    bool found = SymbolEqualityComparer.Default.Equals
+                    (
+                        sm.GetTypeInfo(node, cancellation).Type,
+                        compilation.GetTypeByMetadataName(typeof(TAttribute).FullName)
+                    );
+                    if (found) yield return node;
+                }
+            }
+        }
+    }
+}
---
+++ TEST/ProxyGen.Tests/ProxyEmbedder.cs
@@ -0,0 +1,49 @@
+﻿/********************************************************************************
+* ProxyEmbedder.cs                                                              *
+*                                                                               *
+* Author: Denes Solti                                                           *
+********************************************************************************/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using NUnit.Framework;
+
+namespace Solti.Utils.Proxy.Internals.Tests
+{
+    using Abstractions;
+    using Generators;
+
+    [TestFixture]
+    public class ProxyEmbedderTests
+    {
+        [Test]
+        public void GetAttributes_ShouldReturnAllTheAttributes() 
+        {
+            IProxySyntaxFactory fact = new ProxyGenerator<IList<object>, InterfaceInterceptor<IList<object>>>().SyntaxFactory;
+
+            (CompilationUnitSyntax unit, IReadOnlyCollection<MetadataReference> references, IReadOnlyCollection<Type> _) = fact.GetContext();
+
+            CSharpCompilation compilation = CSharpCompilation.Create
+            (
+                assemblyName: fact.AssemblyName,
+                syntaxTrees: new[]
+                {
+                    CSharpSyntaxTree.Create
+                    (
+                        root: unit
+                    )
+                },
+                references: references,
+                options: CompilationOptionsFactory.Create()
+            );
+
+            AttributeSyntax[] attributes = ProxyEmbedder.GetAttributes<AssemblyTitleAttribute>(compilation).ToArray();
+            Assert.That(attributes.Length, Is.EqualTo(1));
+        }
+    }
+}
