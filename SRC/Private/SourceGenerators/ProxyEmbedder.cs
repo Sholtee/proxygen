8a08ece96316dce73016ffb52c7efb751292cffb
Dénes Solti <sodnaatx@gmail.com>
2020-11-20 14:08:15 +0100

    ProxyEmbedder: introduce GetAOTGenerators() method

--- SRC/Private/SourceGenerators/ProxyEmbedder.cs
+++ SRC/Private/SourceGenerators/ProxyEmbedder.cs
@@ -14,8 +14,13 @@ using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace Solti.Utils.Proxy.Internals
 {
+    using Attributes;
+    using Generators;
+
     internal class ProxyEmbedder
     {
+        private static bool Is(Compilation compilation, ISymbol? s, Type t) => SymbolEqualityComparer.Default.Equals(s, compilation.GetTypeByMetadataName(t.FullName));
+
         public static IEnumerable<AttributeSyntax> GetAttributes<TAttribute>(CSharpCompilation compilation, CancellationToken cancellation = default) where TAttribute : Attribute
         {
             foreach (SyntaxTree syntaxTree in compilation.SyntaxTrees)
@@ -24,12 +29,33 @@ namespace Solti.Utils.Proxy.Internals
 
                 foreach (AttributeSyntax node in syntaxTree.GetRoot(cancellation).DescendantNodes().OfType<AttributeSyntax>())
                 {
-                    bool found = SymbolEqualityComparer.Default.Equals
-                    (
-                        sm.GetTypeInfo(node, cancellation).Type,
-                        compilation.GetTypeByMetadataName(typeof(TAttribute).FullName)
-                    );
-                    if (found) yield return node;
+                    if (Is(compilation, sm.GetTypeInfo(node, cancellation).Type, typeof(TAttribute)))
+                        yield return node;
+                }
+            }
+        }
+
+        public static IEnumerable<(Type Generator, IReadOnlyCollection<ITypeSymbol> GenericArguments)> GetAOTGenerators(CSharpCompilation compilation, CancellationToken cancellation = default) 
+        {
+            foreach(AttributeSyntax attr in GetAttributes<EmbedGeneratedTypeAttribute>(compilation, cancellation))
+            {
+                if (attr.ArgumentList!.Arguments.Single().Expression is TypeOfExpressionSyntax expr) // parametere nem NULL?
+                {
+                    SemanticModel sm = compilation.GetSemanticModel(attr.SyntaxTree);
+
+                    if (sm.GetTypeInfo(expr.Type, cancellation).Type is INamedTypeSymbol named)
+                    {
+                        INamedTypeSymbol genericTypeDefinition = named.OriginalDefinition;
+
+                        Type? generator = genericTypeDefinition switch
+                        {
+                            _ when Is(compilation, genericTypeDefinition, typeof(ProxyGenerator<,>)) => typeof(ProxyGenerator<,>),
+                            _ when Is(compilation, genericTypeDefinition, typeof(DuckGenerator<,>)) => typeof(DuckGenerator<,>),
+                            _ => null
+                        };
+
+                        if (generator is not null) yield return (generator, named.TypeArguments);
+                    }
                 }
             }
         }
--- TEST/ProxyGen.Tests/ProxyEmbedder.cs
+++ TEST/ProxyGen.Tests/ProxyEmbedder.cs
@@ -11,12 +11,18 @@ using System.Reflection;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
+
 using NUnit.Framework;
+
+using Solti.Utils.Proxy;
+using Solti.Utils.Proxy.Attributes;
+using Solti.Utils.Proxy.Generators;
+
+[assembly: EmbedGeneratedType(typeof(IList<>)), EmbedGeneratedType(typeof(ProxyGenerator<IList<int>, InterfaceInterceptor<IList<int>>>))]
 
 namespace Solti.Utils.Proxy.Internals.Tests
 {
     using Abstractions;
-    using Generators;
 
     [TestFixture]
     public class ProxyEmbedderTests
@@ -44,6 +50,44 @@ namespace Solti.Utils.Proxy.Internals.Tests
 
             AttributeSyntax[] attributes = ProxyEmbedder.GetAttributes<AssemblyTitleAttribute>(compilation).ToArray();
             Assert.That(attributes.Length, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void GetAOTGenerators_ShouldReturnAllValidGeneratorsFromAnnotations() 
+        {
+            CSharpCompilation compilation = CSharpCompilation.Create
+            (
+                assemblyName: "cica",
+                syntaxTrees: new[]
+                {
+                    CSharpSyntaxTree.ParseText
+                    (
+                        @"
+                        using System.Collections.Generic;
+
+                        using Solti.Utils.Proxy;
+                        using Solti.Utils.Proxy.Attributes;
+                        using Solti.Utils.Proxy.Generators;
+
+                        [assembly: EmbedGeneratedType(typeof(IList<>)), EmbedGeneratedType(typeof(ProxyGenerator<IList<int>, InterfaceInterceptor<IList<int>>>))]
+                        "
+                    )
+                },
+                references: AppDomain
+                    .CurrentDomain
+                    .GetAssemblies()
+                    .Where(asm => !asm.IsDynamic && !string.IsNullOrEmpty(asm.Location))
+                    .Select(asm => MetadataReference.CreateFromFile(asm.Location)),
+                options: CompilationOptionsFactory.Create()
+            );
+
+            var res = ProxyEmbedder.GetAOTGenerators(compilation).ToArray();
+
+            Assert.That(res.Length, Is.EqualTo(1));
+            Assert.That(res.Single().Generator, Is.EqualTo(typeof(ProxyGenerator<,>)));
+            Assert.That(res[0].GenericArguments.Count, Is.EqualTo(2));
+            Assert.That(res[0].GenericArguments.First().ToDisplayString(), Is.EqualTo("System.Collections.Generic.IList<int>"));
+            Assert.That(res[0].GenericArguments.Last().ToDisplayString(), Is.EqualTo("Solti.Utils.Proxy.InterfaceInterceptor<System.Collections.Generic.IList<int>>"));
         }
     }
 }
