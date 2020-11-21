f198e444c07fb98e3cb30f790df2d2e4c74598c4
Dénes Solti <sodnaatx@gmail.com>
2020-11-21 07:41:44 +0100

    introduce INamedTypeSymbolExtensions

---
+++ SRC/Private/Extensions/INamedTypeSymbolExtensions.cs
@@ -0,0 +1,14 @@
+﻿/********************************************************************************
+* INamedTypeSymbolExtensions.cs                                                 *
+*                                                                               *
+* Author: Denes Solti                                                           *
+********************************************************************************/
+using Microsoft.CodeAnalysis;
+
+namespace Solti.Utils.Proxy.Internals
+{
+    internal static class INamedTypeSymbolExtensions
+    {
+        public static bool IsInterface(this INamedTypeSymbol src) => src.TypeKind == TypeKind.Interface;
+    }
+}
