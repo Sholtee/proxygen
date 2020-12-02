a39fb5badb580b3673bbb96db74655dcead6f9fd
Dénes Solti <sodnaatx@gmail.com>
2020-12-02 10:29:51 +0100

    introduce INamedTypeSymbolExtensions.GetFriendlyName() method

--- SRC/Private/Extensions/INamedTypeSymbolExtensions.cs
+++ SRC/Private/Extensions/INamedTypeSymbolExtensions.cs
@@ -10,5 +10,17 @@ namespace Solti.Utils.Proxy.Internals
     internal static class INamedTypeSymbolExtensions
     {
         public static bool IsInterface(this INamedTypeSymbol src) => src.TypeKind == TypeKind.Interface;
+
+        public static string GetFriendlyName(this INamedTypeSymbol src)
+        {
+            if (src.ContainingType is not null) return src.Name;
+
+            return $"{src.ContainingNamespace}.{src.Name}";
+        }
+
+        public static bool IsGenericTypeDefinition(this INamedTypeSymbol src) => src.IsUnboundGenericType;
+/*
+        public static IEnumerable<ITypeSymbol> GetOwnGenericArguments(this INamedTypeSymbol src) => src.TypeArguments;
+*/
     }
 }
---
+++ TEST/ProxyGen.Tests/ITypeSymbolExtensions.cs
@@ -0,0 +1,96 @@
+﻿/********************************************************************************
+* INamedTypeSymbolExtensions.cs                                                 *
+*                                                                               *
+* Author: Denes Solti                                                           *
+********************************************************************************/
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+
+using NUnit.Framework;
+
+namespace Solti.Utils.Proxy.Internals.Tests
+{
+    [TestFixture]
+    public class INamedTypeSymbolExtensionsTests
+    {
+        private static CSharpCompilation CreateCompilation(string src, params Assembly[] additionalReferences) => CSharpCompilation.Create
+        (
+            "cica",
+            new[]
+            {
+                CSharpSyntaxTree.ParseText(src)
+            },
+            Runtime.Assemblies.Concat(additionalReferences).Distinct().Select(asm => MetadataReference.CreateFromFile(asm.Location!))
+        );
+
+
+        [Test]
+        public void GetFriendlyName_ShouldNotReturnNamespaceForNestedTypes()
+        {
+            CSharpCompilation compilation = CreateCompilation
+            (@"
+                namespace Cica.Mica
+                {
+                    public class GenericParent<T>
+                    {
+                        public class GenericChild<TT>
+                        {
+                        }
+                    }
+                }
+            ");
+
+            var visitor = new FindAllTypesVisitor();
+            visitor.VisitNamespace(compilation.GlobalNamespace);
+
+            INamedTypeSymbol 
+                parent = visitor.AllTypeSymbols.Single(m => m.Name == "GenericParent"),
+                child = visitor.AllTypeSymbols.Single(m => m.Name == "GenericChild");
+
+            Assert.That(parent.GetFriendlyName(), Is.EqualTo("Cica.Mica.GenericParent"));
+            Assert.That(child.GetFriendlyName(), Is.EqualTo("GenericChild"));
+        }
+
+        [Test]
+        public void GetFriendlyName_ShouldWorkWithTuples() 
+        {
+            CSharpCompilation compilation = CreateCompilation
+            (@"
+                public interface IMyInterface: IList<(string Cica, int Mica)>
+                {
+                }
+            ");
+
+            var visitor = new FindAllTypesVisitor();
+            visitor.VisitNamespace(compilation.GlobalNamespace);
+
+            INamedTypeSymbol tuple = (INamedTypeSymbol) visitor.AllTypeSymbols.Single(m => m.Name == "IMyInterface").Interfaces[0].TypeArguments.Single();
+
+            Assert.That(tuple.GetFriendlyName(), Is.EqualTo("System.ValueTuple"));
+        }
+
+        private class FindAllTypesVisitor : SymbolVisitor
+        {
+            public List<INamedTypeSymbol> AllTypeSymbols { get; } = new List<INamedTypeSymbol>();
+
+            public override void VisitNamespace(INamespaceSymbol symbol)
+            {
+                Parallel.ForEach(symbol.GetMembers(), s => s.Accept(this));
+            }
+
+            public override void VisitNamedType(INamedTypeSymbol symbol)
+            {
+                AllTypeSymbols.Add(symbol);
+                foreach (var childSymbol in symbol.GetTypeMembers())
+                {
+                    base.Visit(childSymbol);
+                }
+            }
+        }
+    }
+}
