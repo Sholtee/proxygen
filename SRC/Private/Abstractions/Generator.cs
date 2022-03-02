c31ee53e40b5bf466189df84d0e1d9df1519c6b0
Dénes Solti <sodnaatx@gmail.com>
2022-03-02 19:16:42 +0100

    Generator.cs: implement cancellation properly

--- SRC/Private/Abstractions/Generator.cs
+++ SRC/Private/Abstractions/Generator.cs
@@ -25,9 +25,41 @@ namespace Solti.Utils.Proxy.Internals
         // meghivasra kerulhet (MSDN) -> Lazy
         //
 
-        private static readonly ConcurrentDictionary<Generator, Lazy<Type>> FGeneratedTypes = new(GeneratorComparer.Instance);
+        private static readonly ConcurrentDictionary<Generator, Lazy<Task<Type>>> FGeneratedTypes = new(GeneratorComparer.Instance);
 
         private static readonly ConcurrentDictionary<Type, Lazy<ProxyActivator.Activator>> FActivators = new();
+
+        //
+        // Mivel a Task minden metodusa szal biztos (https://docs.microsoft.com/en-us/dotnet/api/system.threading.tasks.task?view=net-6.0#thread-safety) ezert nem
+        // gond ha ugyanazon a peldanyon osztozunk.
+        //
+
+        internal Task<Type> GetGeneratedTypeAsyncInternal() => FGeneratedTypes.GetOrAdd
+        (
+            this,
+            new Lazy<Task<Type>>
+            (
+                () => Task<Type>.Factory.StartNew
+                (
+                    () => 
+                    {
+                        foreach (ITypeResolution resolution in SupportedResolutions)
+                        {
+                            //
+                            // Megszakitast itt nem adhatunk at mivel az a factoryaba agyazodna -> Ha egyszer
+                            // megszakitasra kerul a fuggveny onnantol soha tobbet nem lehetne hivni.
+                            //
+
+                            Type? resolved = resolution.TryResolve(default);
+                            if (resolved is not null)
+                                return resolved;
+                        }
+                        throw new NotSupportedException();
+                    }
+                ),
+                LazyThreadSafetyMode.ExecutionAndPublication
+            )
+        ).Value;
 
         /// <summary>
         /// Returns the supported type resolution strategies.
@@ -39,32 +71,24 @@ namespace Solti.Utils.Proxy.Internals
         /// Gets the generated <see cref="Type"/> asynchronously .
         /// </summary>
         /// <remarks>The returned <see cref="Type"/> is generated only once.</remarks>
-        public Task<Type> GetGeneratedTypeAsync(CancellationToken cancellation = default) => Task.FromResult(GetGeneratedType()); // TODO
+        public Task<Type> GetGeneratedTypeAsync(CancellationToken cancellation = default)
+        {
+            TaskCompletionSource<Type> tcs = new();
+
+            //
+            // Ha a megszakitas mar kerelmezve lett akkor a SetCanceled() azonnal meghivasra kerul
+            //
+
+            cancellation.Register(tcs.SetCanceled);
+
+            return Task.WhenAny(GetGeneratedTypeAsyncInternal(), tcs.Task).Unwrap();
+        }
 
         /// <summary>
         /// Gets the generated <see cref="Type"/>.
         /// </summary>
         /// <remarks>The returned <see cref="Type"/> is generated only once.</remarks>
-        public Type GetGeneratedType() => FGeneratedTypes.GetOrAdd
-        (
-            this,
-            new Lazy<Type>(() =>
-            {
-                foreach (ITypeResolution resolution in SupportedResolutions)
-                {
-                    //
-                    // TODO: FIXME:
-                    //     Megszakitast itt nem adhatunk at mivel az a Lazy factoryaba agyazodna -> Ha egyszer
-                    //     megszakitasra kerul a fuggveny onnantol soha tobbet nem lehetne hivni.
-                    //
-
-                    Type? resolved = resolution.TryResolve(default);
-                    if (resolved is not null)
-                        return resolved;
-                }
-                throw new NotSupportedException();
-            })
-        ).Value;
+        public Type GetGeneratedType() => GetGeneratedTypeAsyncInternal().Result;
 
         /// <summary>
         /// Creates an instance of the generated type.
