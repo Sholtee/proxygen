72f0204f9da2ada52239c7ee170f599061dd9e8d
Dénes Solti <sodnaatx@gmail.com>
2022-03-17 16:03:38 +0100

    add some new perf tests

--- PERF/DuckGenerator.cs
+++ PERF/DuckGenerator.cs
@@ -6,14 +6,18 @@
 using System;
 
 using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Engines;
 
 namespace Solti.Utils.Proxy.Perf
 {
     using Generators;
 
     [MemoryDiagnoser]
+    [SimpleJob(RunStrategy.Throughput, invocationCount: 30000)]
     public class DuckGenerator
     {
+        private const int OPERATIONS_PER_INVOKE = 100;
+
         public class Implementation
         {
             public int DoSomething(string param) => param.GetHashCode();
@@ -23,5 +27,25 @@ namespace Solti.Utils.Proxy.Perf
         public void AssemblingProxyType() => DuckGenerator<IInterface, Implementation>
             .Instance
             .Emit(Guid.NewGuid().ToString(), default, default);
+
+        [Benchmark(OperationsPerInvoke = OPERATIONS_PER_INVOKE)]
+        public void GetGeneratedType()
+        {
+            for (int i = 0; i < OPERATIONS_PER_INVOKE; i++)
+            {
+                DuckGenerator<IInterface, Implementation>.GetGeneratedType();
+            }
+        }
+
+        private static readonly Implementation FImplementation = new();
+
+        [Benchmark(OperationsPerInvoke = OPERATIONS_PER_INVOKE)]
+        public void Activate()
+        {
+            for (int i = 0; i < OPERATIONS_PER_INVOKE; i++)
+            {
+                DuckGenerator<IInterface, Implementation>.Activate(Tuple.Create(FImplementation));
+            }
+        }
     }
 }
--- PERF/ProxyGenerator.cs
+++ PERF/ProxyGenerator.cs
@@ -6,14 +6,18 @@
 using System;
 
 using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Engines;
 
 namespace Solti.Utils.Proxy.Perf
 {
     using Generators;
 
     [MemoryDiagnoser]
+    [SimpleJob(RunStrategy.Throughput, invocationCount: 30000)]
     public class ProxyGenerator
     {
+        private const int OPERATIONS_PER_INVOKE = 100;
+
         public class InterfaceProxy: InterfaceInterceptor<IInterface>
         {
             public InterfaceProxy(IInterface target) : base(target)
@@ -25,5 +29,23 @@ namespace Solti.Utils.Proxy.Perf
         public void AssemblingProxyType() => ProxyGenerator<IInterface, InterfaceProxy>
             .Instance
             .Emit(Guid.NewGuid().ToString(), default, default);
+
+        [Benchmark(OperationsPerInvoke = OPERATIONS_PER_INVOKE)]
+        public void GetGeneratedType()
+        {
+            for (int i = 0; i < OPERATIONS_PER_INVOKE; i++)
+            {
+                ProxyGenerator<IInterface, InterfaceProxy>.GetGeneratedType();
+            }
+        }
+
+        [Benchmark(OperationsPerInvoke = OPERATIONS_PER_INVOKE)]
+        public void Activate()
+        {
+            for (int i = 0; i < OPERATIONS_PER_INVOKE; i++)
+            {
+                ProxyGenerator<IInterface, InterfaceProxy>.Activate(Tuple.Create((IInterface) null));
+            }
+        }
     }
 }
