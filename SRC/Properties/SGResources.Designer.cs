c438fb4b28f9d34442c2ea2decd170b11fc6a5a6
Dénes Solti <sodnaatx@gmail.com>
2022-03-21 15:24:04 +0100

    update resource [ProxyEmbedder requires C# 8+ compilation]

--- SRC/Properties/SGResources.Designer.cs
+++ SRC/Properties/SGResources.Designer.cs
@@ -19,7 +19,7 @@ namespace Solti.Utils.Proxy.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class SGResources {
@@ -61,7 +61,7 @@ namespace Solti.Utils.Proxy.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Type embedding is supported only in C# 7.0+ compilations..
+        ///   Looks up a localized string similar to Type embedding is supported only in C# 8+ compilations..
         /// </summary>
         internal static string LNG_NOT_SUPPORTED {
             get {
