6f469932740974dc960f32106f1ea4aae6c2ab10
Dénes Solti <sodnaatx@gmail.com>
2022-04-24 09:33:41 +0200

    introduce ClassSyntaxFactoryBase.ResolveStaticGlobal() method

---
+++ SRC/Private/SyntaxFactories/ClassSyntaxFactoryBase.Field.cs
@@ -0,0 +1,58 @@
+﻿/********************************************************************************
+* ClassSyntaxFactoryBase.Field.cs                                               *
+*                                                                               *
+* Author: Denes Solti                                                           *
+********************************************************************************/
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+using static Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
+
+namespace Solti.Utils.Proxy.Internals
+{
+    internal partial class ClassSyntaxFactoryBase
+    {
+        /// <summary>
+        /// private static readonly System.Object paramName [= ...];
+        /// </summary>
+        #if DEBUG
+        internal
+        #endif
+        protected FieldDeclarationSyntax ResolveStaticGlobal(ITypeInfo type, string name, ExpressionSyntax? initializer = null)
+        {
+            VariableDeclaratorSyntax declarator = VariableDeclarator
+            (
+                identifier: Identifier(name)
+            );
+
+            if (initializer is not null) declarator = declarator.WithInitializer
+            (
+                initializer: EqualsValueClause
+                (
+                    initializer
+                )
+            );
+
+            return FieldDeclaration
+            (
+                declaration: VariableDeclaration
+                (
+                    type: ResolveType(type),
+                    variables: SingletonSeparatedList(declarator)
+                )
+            )
+            .WithModifiers
+            (
+                TokenList
+                (
+                    new[]
+                    {
+                        Token(SyntaxKind.PrivateKeyword),
+                        Token(SyntaxKind.StaticKeyword),
+                        Token(SyntaxKind.ReadOnlyKeyword)
+                    }
+                )
+            );
+        }
+    }
+}
