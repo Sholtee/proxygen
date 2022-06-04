fddc8a59a1dd8f7aa62aafbc9d9ff3a180c75325
Dénes Solti <sodnaatx@gmail.com>
2022-06-04 08:43:51 +0200

    ProxyEmbedder: incremental generator support III

--- SRC/Private/SourceGenerators/ProxyEmbedder.cs
+++ SRC/Private/SourceGenerators/ProxyEmbedder.cs
@@ -10,7 +10,12 @@ namespace Solti.Utils.Proxy.Internals
     #pragma warning disable CS3016 // Arrays as attribute arguments is not CLS-compliant
     [Generator(LanguageNames.CSharp)]
     #pragma warning restore CS3016
-    internal sealed class ProxyEmbedder: ProxyEmbedder_RoslynV3
+    internal sealed class ProxyEmbedder:
+#if SRCGEN_V2_API
+        ProxyEmbedder_RoslynV4
+#else
+        ProxyEmbedder_RoslynV3
+#endif
     {
     }
 }
