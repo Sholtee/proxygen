fd4694c61afab4c2567b77521019633d9c58e1c2
Dénes Solti <sodnaatx@gmail.com>
2022-11-23 07:58:12 +0100

    fix release build

--- SRC/Public/InvocationContext.cs
+++ SRC/Public/InvocationContext.cs
@@ -17,6 +17,7 @@ namespace Solti.Utils.Proxy
         /// <summary>
         /// Creates a new <see cref="InvocationContext"/> instance.
         /// </summary>
+        [SuppressMessage("ApiDesign", "RS0016:Add public types and members to the declared API", Justification = "This method exists in debug builds only")]
         public InvocationContext(object?[] args, Func<object, object?[], object?> dispatch): base(dispatch, null)
             => Args = args ?? throw new ArgumentNullException(nameof(args));
 #endif
